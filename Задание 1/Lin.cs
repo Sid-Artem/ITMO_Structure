3429c45e37f76926434cfb6eed02c8d228c897f7
Сидоров Артем <sidoroffartemii@gmail.com>
2024-08-14 21:29:02 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Lin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    public struct Lin
+    {
+        
+        double k;
+        double b;
+
+        public  Lin (double k, double b)
+            
+        {  
+           this.k = k;
+           this.b = b;
+      
+        }
+        public string Root()
+        {
+            double x= -b / k;
+            return $"Решение уравнения = {x}";
+        }
+    }
+    
+}
---
+++ Задание 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+
+            Console.WriteLine("Введите коэфициент К");
+            double K= double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите коэфициент B");
+            double B= double.Parse(Console.ReadLine());
+            Lin lin = new Lin(K, B);
+            Console.WriteLine(lin.Root());
+            Console.ReadLine();
+        }
+    }
+}
